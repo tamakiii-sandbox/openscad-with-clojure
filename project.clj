6fdbd130966e0d14cbfc33f568d70ba0d83147e9
tamakiii <tamaki.htm@gmail.com>
2018-08-19 23:07:30 +0900

    add project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+ (defproject oenscad-clojure "0,0,0-SNAPSHOT"
+  :description "OpenSCAD with Clojure using scad-clj"
+  :url "https://github.com/tamakiii-sandbox/openscad-with-clojure"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [scad-clj "0.5.3"]])
